4b40e416c2ad54ed5da48401d1b26aac3a594ef0
Divya Narayanan <divya@ashokn.org>
2022-05-01 12:58:30 -0400

    added images, imgrec is now using inRange to process image

--- imgrec.py
+++ imgrec.py
@@ -1,51 +1,46 @@
 import cv2 as cv
-
-threshold = 150
-
-def isWhite(pixel):
-    #print(pixel)
-    if (pixel[0] > threshold and pixel[1] > threshold and pixel[2] > threshold):
-        return True
-    return False
-
-# vid = cv.VideoCapture(0)
+import numpy as np
 
 org = (50,50)
 
-color = (255, 0, 0)
+imageList = {'land_water.jpg', 'land.jpg', 'water_plastic_at_edge.jpg', 'water_plastic1.jpg', 'water_plastic2.jpg', 'water.jpg'}
 
-# while(True):
-#     ret, frame = vid.read()
-#     if ret == True:
-#         cv.imshow('image', frame)
-#         if  cv.waitKey(1) & 0xFF == ord('q'):
-#             break
+lowH = 90
+highH = 120
+lowS = 0
+highS = 255
+lowV = 0
+highV = 255
 
-#print (str(image[i,j][0]) + ' ' + str(image[i,j][1]) + ' ' + str(image[i,j][2]))
+minWaterPercent = 20.0
+minPlasticPercent = 94.0
+maxPlasticPercent = 99.0
 
+def percentWater(filter):
+    waterPixels = np.sum(filter == 255)
+    nonWaterPixels = np.sum(filter == 0)
+    waterPercent = round((100*waterPixels)/(waterPixels + nonWaterPixels), 3)
+    return waterPercent
 
-# vid.release()
-
-image = cv.imread('PXL_2.jpg')
-print('Loaded image')
-cv.imshow('pic', image)
-print('showed image')
-
-(h, w, c) = image.shape[:3]
-
-for i in range(h):
-    for j in range(w):
-        if(isWhite(image[i, j])):
-            image[i, j][0] = 0
-            image[i, j][1] = 0
-            image[i, j][2] = 255
-
-cv.imshow('pic2', image)
-print('showed image 2')
-
+def findType(percent):
+    if (percent < minWaterPercent):
+        return 'LAND'
+    elif (percent > maxPlasticPercent or percent < minPlasticPercent):
+        return 'WATER'
+    else:
+        return 'PLASTIC'
+    
+for imgName in imageList:
+    image = cv.imread('images/' + imgName)
+    #cv.imshow(imgName, image)
+    hsvImage = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+    #cv.imshow(imgName, hsvImage)
+    filter = cv.inRange(hsvImage, (lowH, lowS, lowV), (highH, highS, highV))
+    cv.imshow(imgName, filter)
+    percent = percentWater(filter)
+    print(str(imgName) + ' is most likely ' + str(findType(percent)) + ' with ' + str(percent) + "% water found")
 
 while True:
-
     # quit if 'q' is pressed
     if  cv.waitKey(60) & 0xFF == ord('q'):
         break
